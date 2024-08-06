78a226c5be78425eaaafb763dc62b9c090a2de7c
Precious Adedibu <adedibuprecious@gmail.com>
2024-08-06 10:46:06 +0100

    update

--- components/landingpage/home/Courses.tsx
+++ components/landingpage/home/Courses.tsx
@@ -52,37 +52,39 @@ const Courses = () => {
             The world is your classroom. Explore, reflect, and learn how you can
             move a step forward in your career and life.
           </p>
-          <div className="grid grid-cols-1 iphone:grid-cols-2 gap-12 mt-12 sm:grid-cols-3">
-            {courses.slice(0, 6).map((item, index) => (
-              <Link
-                href={`/courses/${item.id}`}
-                key={index}
-                className={`border border-borderGrey rounded-lg px-3 py-3 hover:shadow-form`}
-              >
-                <div className="w-full h-[15.75rem] relative">
-                  <Image
-                    src={item.thumbnail_image}
-                    alt={item.title}
-                    className=" rounded-tl-lg rounded-tr-lg rounded-bl-primary rounded-br-primary"
-                    width={0}
-                    height={0}
-                    sizes="100vw"
-                    style={{ width: "100%", height: "100%" }} // optional
-                  />
-                </div>
-                <div className="py-3">
-                  <h2 className="font-medium text-xl leading-fourth">
-                    {item.title}
+          <div className="flex justify-center">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-12 mt-12 md:grid-cols-3 justify-items-center">
+              {courses.slice(0, 6).map((item, index) => (
+                <Link
+                  href={`/courses/${item.id}`}
+                  key={index}
+                  className={`border border-borderGrey rounded-lg px-3 py-3 hover:shadow-form`}
+                >
+                  <div className="w-full h-[15.75rem] relative">
+                    <Image
+                      src={item.thumbnail_image}
+                      alt={item.title}
+                      className=" rounded-tl-lg rounded-tr-lg rounded-bl-primary rounded-br-primary"
+                      width={0}
+                      height={0}
+                      sizes="100vw"
+                      style={{ width: "100%", height: "100%" }} // optional
+                    />
+                  </div>
+                  <div className="py-3">
+                    <h2 className="font-medium text-xl leading-fourth">
+                      {item.title}
+                    </h2>
+                    <h4 className="text-sm leading-seventh text-greyText">
+                      Earn a certificate
+                    </h4>
+                  </div>
+                  <h2 className="text-end text-primary text-sm leading-seventh">
+                    View Course
                   </h2>
-                  <h4 className="text-sm leading-seventh text-greyText">
-                    Earn a certificate
-                  </h4>
-                </div>
-                <h2 className="text-end text-primary text-sm leading-seventh">
-                  View Course
-                </h2>
-              </Link>
-            ))}
+                </Link>
+              ))}
+            </div>
           </div>
           <div className="flex justify-center mt-12">
             <div className="bg-primary w-[15rem] h-[3.75rem] py-2 rounded-btn flex items-center justify-center">
