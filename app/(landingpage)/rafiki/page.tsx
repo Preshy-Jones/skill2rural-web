a2f5e7fe921c308a565dea387c6d4d17204db992
Pelumiii1 <adesokanpelumi14@gmail.com>
2025-03-01 19:31:27 +0100

    update

---
+++ app/(landingpage)/rafiki/page.tsx
@@ -0,0 +1,7 @@
+import Hero from "@/components/landingpage/rafiki/Hero"
+
+export default function Rafiki(){
+    return(
+      <Hero />
+    )
+}
--- components/landingpage/home/NavBar/NavBar.tsx
+++ components/landingpage/home/NavBar/NavBar.tsx
@@ -46,7 +46,7 @@ const NavBar = () => {
           <Link href={"/"}>
             <h3 className="mr-12 font-semibold leading-fifth">Home</h3>
           </Link>
-          <Link href={"/"} className='relative'>
+          <Link href={"/rafiki"} className='relative'>
             <span className="text-primary text-sm font-bold absolute top-[-15px] right-6">New</span>
             <h3 className="mr-12 font-semibold leading-fifth">Rafiki</h3>
           </Link>
