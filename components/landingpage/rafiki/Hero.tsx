1e90c442cd572b8d937d92808b8461b3e16720ce
Pelumiii1 <adesokanpelumi14@gmail.com>
2025-04-14 06:48:48 +0100

    bug fixes

--- components/landingpage/rafiki/Hero.tsx
+++ components/landingpage/rafiki/Hero.tsx
@@ -28,8 +28,8 @@ export default function Hero() {
         </div>
       </div>
 
-      <div className="z-40 flex justify-center mt-8">
-        <button className="bg-primary text-white w-[15rem] h-[3.75rem] py-2 rounded-btn font-semibold sm:text-base text-sm sm:leading-fifth leading-seventh">
+      <div className="z-40 flex justify-center mt-8 relative">
+        <button className="bg-primary text-white w-[15rem] h-[3.75rem] py-2 rounded-btn font-semibold sm:text-base text-sm sm:leading-fifth leading-seventh relative cursor-pointer">
           <a
             href={
               "https://wa.me/+15557228341?text=Hello%20Rafiki%2C%20I%20need%20career%20guidance"
